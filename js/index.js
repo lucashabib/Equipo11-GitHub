c5f1a5b1502625f332f740410103de0390c3fc25
BenjaOrchansky <borchansky@udesa.edu.ar>
2024-06-19 17:31:29 -0300

    categoria electronica del index modificada y js del index creado,70% hecho

---
+++ js/index.js
@@ -0,0 +1,30 @@
+let electronica = document.querySelectorAll(".Contenedor_electronica");
+
+fetch("https://fakestoreapi.com/products/category/electronics")
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(info){
+        let retorno = info;
+        console.log(info);
+
+        for(let i = 0; i < retorno.length; 1++){
+            if(i < electronica.length){
+                let nombreProd = retorno[i].title;
+                let precioProd = retorno[i].price;
+                let idProd = retorno[i].id;
+                let descripProd = retorno[i].description;
+                let imgProd = retorno[i].image;
+                let nombre = electronica[i].querySelector(".titulo_producto")
+                let precio = electronica[i].querySelector(".precio_producto")
+                let descripcion = electronica[i].querySelector(".descripcion_producto")
+                let imagen = electronica[i].querySelector(".Imagen_Electronica")
+                let boton = electronica[i].querySelector(".Detalles_Producto")
+                nombre.innerText
+
+            }
+        }
+    })        
+    .catch(function(error){
+        console.error('Error:', error);
+    });
