41df9d860d1035a23974e1204252346ee60f446e
BenjaOrchansky <borchansky@udesa.edu.ar>
2024-06-19 21:36:15 -0300

    electronica terminada

--- js/index.js
+++ js/index.js
@@ -8,23 +8,29 @@ fetch("https://fakestoreapi.com/products/category/electronics")
         let retorno = info;
         console.log(info);
 
-        for(let i = 0; i < retorno.length; 1++){
+        for (let i = 0; i < retorno.length; i++){
             if(i < electronica.length){
                 let nombreProd = retorno[i].title;
                 let precioProd = retorno[i].price;
                 let idProd = retorno[i].id;
                 let descripProd = retorno[i].description;
                 let imgProd = retorno[i].image;
-                let nombre = electronica[i].querySelector(".titulo_producto")
-                let precio = electronica[i].querySelector(".precio_producto")
-                let descripcion = electronica[i].querySelector(".descripcion_producto")
-                let imagen = electronica[i].querySelector(".Imagen_Electronica")
-                let boton = electronica[i].querySelector(".Detalles_Producto")
-                nombre.innerText
+                let nombre = electronica[i].querySelector(".titulo_producto");
+                let precio = electronica[i].querySelector(".precio_producto");
+                let descripcion = electronica[i].querySelector(".descripcion_producto");
+                let imagen = electronica[i].querySelector(".Imagen_Electronica");
+                let boton = electronica[i].querySelector(".Detalles_Producto");
+                nombre.innerText = nombreProd;
+                precio.innerText = `$${precioProd}`;
+                imagen.src = imgProd;
+                descripcion.innerText = descripProd;
+
+
+
 
             }
         }
     })        
     .catch(function(error){
-        console.error('Error:', error);
+        console.log('Error:', error);
     });
